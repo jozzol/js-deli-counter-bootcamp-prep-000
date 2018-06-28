39eeab6db9a4ddd06a168a9579edd982de32c3d9
Jose Guillermo Garcia Serrano <joseg1224@hotmail.com>
2018-06-28 18:58:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(line){
 }
 function currentLine(line){
   if(!line.length){
-    console.log("The line is currently empty")
-    return "The line is currently empty"
+    console.log("The line is currently empty.")
+    return "The line is currently empty."
   }
 }
