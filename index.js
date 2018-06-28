7748c66ef64d15cccb371ce2864bc3de594ae1bf
Jose Guillermo Garcia Serrano <joseg1224@hotmail.com>
2018-06-28 19:05:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,6 @@ function currentLine(line){
   for (var i = 0; i < 4; i++){
     nameAndNumbers.push(i+1 + "." + line[i])
   }
-  console.log("The line is currently "+ nameAndNumbers)
+  console.log("The line is currently "+ nameAndNumbers + ".")
+  return "The line is currently "+ nameAndNumbers + "."
 }
