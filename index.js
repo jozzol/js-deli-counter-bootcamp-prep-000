6923bbfd32c1d5eca924e03676af362a80f1d061
Jose Guillermo Garcia Serrano <joseg1224@hotmail.com>
2018-06-28 16:35:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDeli = [];
 function takeANumber(line, name){
   line.push(name)
-  console.log("Welcome,"+ name +"you are")
+  console.log("Welcome, "+ name +"you are number "+ line.length +" in line")
 }
